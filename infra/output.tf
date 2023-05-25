0800da880fd6dfc4ae4aed0d0d9be23401faeadd
Saverio Proto <saverioproto@microsoft.com>
2023-05-25 11:38:27 +0200

    Use AKS workload identity to authenticate to Azure OpenAI

--- infra/output.tf
+++ infra/output.tf
@@ -1,11 +1,11 @@
 output "installation-script" {
-  sensitive = true
   value = templatefile("installation_script.tftpl",
     { resourceGroup = azurerm_resource_group.this.name,
       aksName       = module.aks.aks_name,
       registry      = azurerm_container_registry.acr.name,
       endpoint      = module.openai.openai_endpoint,
-      key           = module.openai.openai_primary_key,
+      clientid      = azurerm_user_assigned_identity.chatbot.client_id,
+      oidc_url      = module.aks.oidc_issuer_url,
     }
   )
 }
