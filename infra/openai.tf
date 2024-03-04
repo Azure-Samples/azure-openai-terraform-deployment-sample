19f14753569254131f732f505e91deab65e85dbb
Saverio Proto <saverioproto@microsoft.com>
2024-03-04 16:46:02 +0100

    Bump "Azure/openai/azurerm" to v0.1.3

--- infra/openai.tf
+++ infra/openai.tf
@@ -1,6 +1,6 @@
 module "openai" {
   source              = "Azure/openai/azurerm"
-  version             = "0.1.1"
+  version             = "0.1.3"
   resource_group_name = azurerm_resource_group.this.name
   location            = azurerm_resource_group.this.location
   private_endpoint = {
